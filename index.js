71b4d5b76eb5997593dce712672511327672e5e7
Jon Bevan <jbevan@adaptavist.com>
2017-04-14 15:46:03 +0100

    Support for simple onError action

--- index.js
+++ index.js
@@ -9,53 +9,120 @@ function RatpackPromise(generator) {
 
     this.generator = generator;
 }
+
+/**
+ * This is a terminating call on the promise chain.
+ */
 RatpackPromise.prototype.then = function(action) {
-    const resolve = (resolvedValue) => {
+    // When executing .then() and we have a successful promise, pass
+    // the value of the promise to the action that does something
+    // with that promise value
+    const resolveHandler = (resolvedValue) => {
         this.error = false;
         this.success = true;
         this.complete = true;
         this.value = resolvedValue;
         action(resolvedValue);
     };
-    const reject = (rejectError) => {
-        // TODO - enforce type
+
+    // When executing .then() and we have an error, don't actually
+    // do anything.
+    const rejectHandler = (rejectError) => {
+        // TODO - enforce type of rejectError?
         this.error = true;
         this.success = false;
-        this.complete = false;
+        this.complete = true;
     };
-    this.generator(resolve.bind(this), reject.bind(this));
+    this.generator(resolveHandler.bind(this), rejectHandler.bind(this));
     return;
 };
-RatpackPromise.prototype.onError = function(type, action) {
 
+/**
+ * This is a terminating call on the promise chain.
+ */
+RatpackPromise.prototype.onError = function(action) {
+    let stageResolved = false;
+    let stageRejected = false;
+    const isStageResolved = () => this.success || stageResolved;
+    const isStageRejected = () => this.error || stageRejected;
+
+    const resolveHandler = resolveValue => {
+        if (isStageResolved()) {
+            console.log('[DEBUG] [onError] Ignoring resolve call. Already resolved.');
+            return;
+        }
+        if (isStageRejected()) {
+            console.log('[DEBUG] [onError] Ignoring resolve call. Already rejected.');
+            return;
+        }
+        this.error = false;
+        this.success = true;
+        this.complete = true;
+    };
+
+    const rejectHandler = rejectError => {
+        if (isStageResolved()) {
+            console.log('[DEBUG] [onError] Ignoring reject call. Already resolved.');
+            return;
+        }
+        if (isStageRejected()) {
+            console.log('[DEBUG] [onError] Ignoring reject call. Already rejected.');
+            return;
+        }
+        this.error = true;
+        this.success = false;
+        this.complete = true;
+        action(rejectError);
+    };
+
+    try {
+        this.generator(resolveHandler.bind(this), rejectHandler.bind(this));
+    } catch (e) {
+        rejectHandler(e);
+    }
+    return this;
 };
-RatpackPromise.prototype.mapError = function(action) {
-    let isStageResolved = false;
-    let isStageRejected = false;
 
-    const resolveHandler = resolve => resolvedValue => {
-        if (isStageResolved) {
-            console.log('Ignoring resolve call. Already resolved.');
+RatpackPromise.prototype.mapError = function(action) {
+    let stageResolved = false;
+    let stageRejected = false;
+    const isStageResolved = () => this.success || stageResolved;
+    const isStageRejected = () => this.error || stageRejected;
+
+    // When executing .mapError(), we want to make sure we haven't executed through this point in
+    // the promise chain already. This function generates a resolveHandler that takes resolvedValue
+    // and checks whether this stage in the promise chain has already been resolved or rejected.
+    // If this stage is unresolve and unrejected, the resolvedValue is just passed on to the resolve
+    // function this builder got passed in.
+    const makeResolveHandler = resolve => resolvedValue => {
+        if (isStageResolved()) {
+            console.log('[DEBUG] [mapError] Ignoring resolve call. Already resolved.');
             return;
         }
-        if (isStageRejected) {
-            console.log('Ignoring resolve call. Already rejected.');
+        if (isStageRejected()) {
+            console.log('[DEBUG] [mapError] Ignoring resolve call. Already rejected.');
             return;
         }
-        isStageResolved = true;
+        stageResolved = true;
         resolve(resolvedValue);
     }
 
-    const rejectHandler = (resolve, reject) => ((rejectError) => {
-        if (isStageResolved) {
-            console.log("Ignoring reject call. Already resolved.");
+    // When executing .mapError(), we want to make sure we haven't executed through this point in
+    // the promise chain already. This function generates a rejectHandler that takes the rejection error
+    // value and as long as this stage is unresolved and unrejected it passes that rejection error value
+    // to the action specified for this mapError call. If that calls succeeds, the newly mapped value
+    // gets passed to the resolve function.
+    // If that call fails, then the reject call is executed.
+    const makeRejectHandler = (resolve, reject) => ((rejectError) => {
+        if (isStageResolved()) {
+            console.log("[DEBUG] [mapError] Ignoring reject call. Already resolved.");
             return;
         }
-        if (isStageRejected) {
-            console.log("Ignoring reject call. Already rejected.");
+        if (isStageRejected()) {
+            console.log("[DEBUG] [mapError] Ignoring reject call. Already rejected.");
             return;
         }
-        isStageRejected = true;
+        stageRejected = true;
         try {
             const newValue = action(rejectError);
             this.error = false;
@@ -75,9 +142,9 @@ RatpackPromise.prototype.mapError = function(action) {
     const currentGenerator = this.generator;
     const newGenerator = (resolve, reject) => {
         try {
-            currentGenerator(resolveHandler(resolve), rejectHandler(resolve, reject));
+            currentGenerator(makeResolveHandler(resolve), makeRejectHandler(resolve, reject));
         } catch (e) {
-            rejectHandler(resolve, reject)(e);
+            makeRejectHandler(resolve, reject)(e);
         }
     };
     this.generator = newGenerator;
@@ -101,10 +168,12 @@ new RatpackPromise((resolve, reject) => {
     setTimeout(() => {
         reject("jello");
     }, 1000);
-    throw new Error('setTimeout is undefined');
+    //throw new Error('setTimeout is undefined');
 }).mapError(err => {
     console.log('Mapping error', err);
     return 'hello';
+}).onError(e => {
+    console.log('onError',e);
 }).mapError(err => {
     console.log('Mapping error', err);
     return 'recovered 2nd mapError';
